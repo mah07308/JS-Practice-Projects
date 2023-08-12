2af2c454ac624267a27546762f638d3921937a58
Ahmad H <mah07308@gmail.com>
2023-08-13 00:45:22 +0530

    follow along

---
+++ 16-BT-Dad-Jokes/script.js
@@ -0,0 +1,36 @@
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  };
+
+  const resp = await fetch('https://icanhazdadjoke.com', config);
+
+  const data = await resp.json();
+
+  jokeEl.innerHTML = data.joke;
+}
+
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com', config)
+//     .then(response => response.json())
+//     .then(data => {
+//       jokeEl.innerHTML = data.joke;
+//     });
+// }
